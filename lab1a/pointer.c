a08820269dded1a4ccf8c340f847a07afd2663c1
lantinglu <161433199+lantinglu@users.noreply.github.com>
2024-05-24 19:09:58 -0700

    Update pointer.c

--- lab1a/pointer.c
+++ lab1a/pointer.c
@@ -2,7 +2,7 @@
  * CSE 351 Lab 1a (Pointers in C)
  *
  * Name(s):
- * NetID(s):
+ * NetID(s): 
  *
  * WARNING: Do not include the <stdio.h> header; it confuses the dlc compiler.
  * You can still use printf for debugging without including <stdio.h>. The
