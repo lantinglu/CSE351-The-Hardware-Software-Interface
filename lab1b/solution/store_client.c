ed6e57be90ab9289077a922009d0db1eb5a9182d
lantinglu <161433199+lantinglu@users.noreply.github.com>
2024-05-24 19:15:29 -0700

    Update store_client.c

--- lab1b/solution/store_client.c
+++ lab1b/solution/store_client.c
@@ -1,8 +1,8 @@
 /*
  * CSE 351 Lab 1b (Manipulating Bits in C)
  *
- * Name(s): Lanting Lu  
- * NetID(s): llu3
+ * Name(s): 
+ * NetID(s): 
  *
  * This is a file for managing a store of various aisles, represented by an
  * array of 64-bit integers. See aisle_manager.c for details on the aisle
